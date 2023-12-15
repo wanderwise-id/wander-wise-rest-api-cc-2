50674a95214752abfa464c3726617cfa5a32ab47
oreoataya <dontrichpeopledifficulut@gmail.com>
2023-12-16 01:37:16 +0800

    add posts method inlcude jwt middleware

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,10 +9,11 @@ const {
 
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
+const { post } = require('../routes/upload');
 
 
 const getAllPosts = async (req, res) => {
-  
+  res.send('get all posts route');
 };
 
 const getPost = async (req, res) => {
@@ -20,65 +21,36 @@ const getPost = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  try {
-    const { uid } = req.session;
+const userId = req.session.uid; // Get UID from Firebase auth middleware
+const { 
+  title, 
+  image, 
+  caption 
+} = req.body;
 
-    if (!uid) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        error: true,
-        msg: 'Authentication invalid',
-        body: null,
-      });
-    }
+const postdoc = postsRef.doc();
 
-    const userRef = usersRef.doc(uid);
-    const userDoc = await userRef.get();
+try {
+  const post = {
+    userId: userId,
+    idPost: postdoc.id,
+    title: title,
+    image: image,
+    caption: caption,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    // Add other post properties here
+  };
 
-    if (!userDoc.exists) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        error: true,
-        msg: 'User not found',
-        body: null,
-      });
-    }
-
-    const userData = userDoc.data();
-    const { username } = userData;
-
-    const newPost = {
-      title: req.body.title,
-      image: req.body.image,
-      description: req.body.description,
-      location: req.body.location,
-      username,
-      userId: uid,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-    };
-
-    const postRef = await db.collection('posts').add(newPost);
-    const postDoc = await postRef.get();
-    const postData = postDoc.data();
-
-    const post = {
-      id: postDoc.id,
-      ...postData,
-    };
-
+    await postdoc.set(post);
     res.status(StatusCodes.CREATED).json({
       error: false,
-      msg: 'Success create new post',
+      msg: 'Post created',
       body: {
         post,
       },
     });
   } catch (error) {
-    console.error('Error creating post:', error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: true,
-      msg: 'Internal Server Error',
-      body: null,
-    });
+    throw new BadRequestError(`Error creating post: ${error}`);
   }
 };
 
